f17e1e5c8c7e65d1cdd476640b09b7230c1349ad
Ray Nirola <hi@raynirola.com>
2021-10-02 12:58:50 +0530

    Typescript Setup

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from "next/app"
+import { ThemeProvider } from "next-themes"
+
+import "tailwindcss/tailwind.css"
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<ThemeProvider attribute="class">
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
+}
+
+export default MyApp
