f7d9b82baee133841bbc95c0e97c0b38123ad1f3
Ray Nirola <hi@raynirola.com>
2021-10-02 19:00:51 +0530

    rework article section, post header and removed sidebar for now

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -5,6 +5,7 @@ import Image from "next/image"
 import { AppLayout } from "@/layouts";
 import { Post } from "@/models";
 import { AllPostSlugQuery, API, GetPostBySlugQuery } from "@/api";
+import { formattedTime } from "@/utils/time";
 
 interface PostPageProps {
     post: Post
@@ -19,27 +20,63 @@ const PostPage: NextPage<PostPageProps> = ({post}) => {
                 <meta name='description' content={post.seo.description}/>
             </Head>
 
-            <article className='px-4 py-8 mx-auto prose'>
-                <header className='mb-8'>
-                    <h1>{post.title}</h1>
-                    <p className='description'>{post.excerpt}</p>
-                    <div className='relative overflow-hidden rounded aspect-w-16 aspect-h-8'>
-                        <Image
-                            src={post.coverImage.url}
-                            layout='fill'
-                            objectFit='cover'
-                        />
+            <main>
+                <header className='max-w-xl px-4 mx-auto md:max-w-3xl lg:max-w-5xl xl:max-w-6xl xl:px-0'>
+                    <div className='py-4 sm:py-8 md:py-10 lg:py-12'>
+                        <div className="max-w-3xl mx-auto mb-8 lg:mb-12">
+                            <nav className="flex flex-wrap font-light text-xs lg:text-base text-gray-500 mb-4">
+                                {post.tags.map(tag => renderTag(tag))}
+                            </nav>
+                            <div className="prose prose-sm sm:prose lg:prose-lg mb-6 min-w-full">
+                                <h1>{post.title}</h1>
+                            </div>
+                            <div className="flex items-center mb-6">
+                                <div
+                                    className="relative overflow-hidden w-8 h-8 rounded-full flex-shrink-0 object-cover object-center">
+                                    <Image
+                                        layout="fill"
+                                        objectFit="cover"
+                                        src={post.author.picture.url}
+                                        alt={post.author.name}
+                                    />
+                                </div>
+                                <div className="pl-3">
+                                    <div className="block text-sm font-semibold text-gray-700">{post.author.name}</div>
+                                    <time
+                                        className="block text-xs text-gray-500 font-light">{formattedTime(post.publishedAt)}</time>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className='relative overflow-hidden rounded aspect-w-5 aspect-h-3 md:aspect-w-16 md:aspect-h-6'>
+                            <Image
+                                src={post.coverImage.url}
+                                layout='fill'
+                                objectFit='cover'
+                            />
+                        </div>
                     </div>
                 </header>
-                <main className='mx-auto'>
-                    <article
-                        className="prose prose-yellow"
-                        dangerouslySetInnerHTML={{__html: post.content.html}}
-                    />
-                </main>
-            </article>
+                <article className="bg-white">
+                    <div className="max-w-xl px-4 mx-auto md:max-w-3xl lg:max-w-5xl xl:max-w-6xl xl:px-0">
+                        <section
+                            className="py-8 max-w-3xl prose lg:prose-lg prose-yellow mx-auto sm:pb-16 md:pb-24 lg:pb-36"
+                            dangerouslySetInnerHTML={{__html: post.content.html}}
+                        />
+                    </div>
+                </article>
+            </main>
         </AppLayout>
     )
+
+    function renderTag(tag: string) {
+        return (
+            <div className="mr-4 mt-2" key={tag}>
+                <span className="text-gray-300">#</span>
+                {tag}
+            </div>
+        )
+    }
 };
 
 export default PostPage
