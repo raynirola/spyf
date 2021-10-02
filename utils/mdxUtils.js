5d795b7eb054deaecc4e14c7b5274a3635662306
Ray Nirola <hi@raynirola.com>
2021-10-02 12:59:11 +0530

    Remove old files

--- utils/mdxUtils.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs"
-import path from "path"
-
-export const postPath = path.join(process.cwd(), "posts")
-
-export const postFilePaths = fs.readdirSync(postPath).filter((path) => /\.mdx?$/.test(path))
